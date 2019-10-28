6af60ed0f323e43557314c512f5c9cb7c7feb490
flikkes <lpke.flx@gmail.com>
2019-10-28 12:38:32 +0100

    initial commit

---
+++ dreamshare/src/main/java/de/hszg/luepke/dreamshare/DreamshareApplication.java
@@ -0,0 +1,13 @@
+package de.hszg.luepke.dreamshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamshareApplication.class, args);
+	}
+
+}
---
+++ dreamshare/src/test/java/de/hszg/luepke/dreamshare/DreamshareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hszg.luepke.dreamshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
