2356ab43773d01c5135a7083be657edb25e37a03
flikkes <lpke.flx@gmail.com>
2019-12-02 00:47:40 +0100

    experimented more with templating a js renderer

--- dreamshare/src/main/java/de/hszg/luepke/dreamshare/rendering/RendererRestController.java
+++ dreamshare/src/main/java/de/hszg/luepke/dreamshare/rendering/RendererRestController.java
@@ -9,11 +9,13 @@ import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.apache.velocity.runtime.resource.loader.FileResourceLoader;
 import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+import org.springframework.boot.SpringApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import de.hszg.luepke.dreamshare.content.ImageEntity;
 
@@ -21,7 +23,7 @@ import de.hszg.luepke.dreamshare.content.ImageEntity;
 @RequestMapping("renderer")
 public class RendererRestController {
 
-    @GetMapping("")
+    @GetMapping("script")
     public ResponseEntity<?> getRenderer(@RequestParam String rendererName) {
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
@@ -31,7 +33,23 @@ public class RendererRestController {
         Template t = ve.getTemplate("templates/defaultRenderer.js.vm");
 
         VelocityContext context = new VelocityContext();
-        context.put("entityClass", ImageEntity.class);
+        context.put("baseUrl", "http://localhost:8080/content/image");
+
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
+        return ResponseEntity.ok(writer.toString());
+    }
+
+    @GetMapping("template")
+    public ResponseEntity<?> getTemplate(@RequestParam String rendererName) {
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
+
+        Template t = ve.getTemplate("templates/defaultRenderer.html.vm");
+
+        VelocityContext context = new VelocityContext();
 
         StringWriter writer = new StringWriter();
         t.merge(context, writer);
